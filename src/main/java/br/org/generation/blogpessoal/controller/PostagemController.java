2e297f8e64bf92635d0b24e54f22e71faa62301f
Gustavo Cardoso <contato.gusttavo08@gmail.com>
2022-01-19 19:14:02 -0300

    Adicionados métodos PutMapping e DeleteMapping ao código

--- src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,11 +61,13 @@ public ResponseEntity <Postagem> postPostagem(@Valid @RequestBody Postagem posta
 
 @PutMapping
 public ResponseEntity <Postagem> putPostagem(@Valid @RequestBody Postagem postagem){
-//return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
-	return postagemRepository.findById(postagem.getId())
-			.map(resposta -> ResponseEntity.ok(postagemRepository.save(postagem)))
-			.orElse(ResponseEntity.notFound().build());
+return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
 	
 }
 
+@DeleteMapping("/{id}")
+public void delete(@PathVariable long id) {
+	postagemRepository.deleteById(id);
+	
+}
 }
